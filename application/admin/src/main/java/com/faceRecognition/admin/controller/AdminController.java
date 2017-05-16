acc82e1ee27c3bd459986ec4dfdd7d261db8d520
kaikun213 <kaktus.w.h@gmx.de>
2017-05-16 10:21:29 +0200

    Delete and update admin

--- application/admin/src/main/java/com/faceRecognition/admin/controller/AdminController.java
+++ application/admin/src/main/java/com/faceRecognition/admin/controller/AdminController.java
@@ -1,17 +1,19 @@
 package com.faceRecognition.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.faceRecognition.admin.api.ApiResponse;
+import com.faceRecognition.admin.api.ApiResponse.ApiError;
 import com.faceRecognition.admin.api.ApiResponse.Status;
 import com.faceRecognition.utils.database.model.UserEntity;
 import com.faceRecognition.utils.database.service.AdminService;
@@ -23,12 +25,12 @@ public class AdminController {
 	@Autowired
 	AdminService databaseService;
 	
-	@RequestMapping(method=RequestMethod.POST)
+	@PostMapping
 	public ApiResponse create(@RequestParam(value = "file", required=true) MultipartFile file,
 			  				  @RequestParam(value = "personalNumber", required=true) String personalNumber){
 		String link = "linkToFile";		// link = storageService.store(file);
 		String faceId = "1";			// faceId = faceService.save(link)	
-		UserEntity userEntity = null; // database should return result
+		UserEntity userEntity = null; 	// database should return result
 		databaseService.addUserEntity(faceId, personalNumber);
 		return new ApiResponse(Status.OK, userEntity);
 	}
@@ -37,6 +39,34 @@ public class AdminController {
 	public ApiResponse retrieve(@PathVariable String id){
 		UserEntity userEntity = databaseService.getUserEntity(id);
 		return new ApiResponse(Status.OK, userEntity);
+	}
+	
+	@PutMapping("/{id}")
+	public ApiResponse update(@RequestParam(value = "file", required=true) MultipartFile file,
+			  				  @RequestParam(value = "personalNumber", required=true) String personalNumber,
+			  				  @PathVariable String id){
+		try {
+			String link = "linkToFile";			// link = storageService.store(file);
+												// faceService.update(id, link);
+			UserEntity userEntity = null;		// database should return result	
+			databaseService.updateUserEntity(id, personalNumber);
+			String faceId = "1";				// faceId = faceService.save(link)	
+			return new ApiResponse(Status.OK, userEntity);
+		} catch (NotFoundException e) {
+			return new ApiResponse(Status.ERROR, null, new ApiError(1, "Entity not found"));
+		}
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ApiResponse delete(@PathVariable String id){
+		try {
+			// faceService.delete(id);
+			databaseService.deleteUserEntity(id);
+			return new ApiResponse(Status.OK, null, null);
+		} catch (NotFoundException e) {
+			return new ApiResponse(Status.ERROR, null, new ApiError(1, "Entity not found."));
+		}
 	}
 
 
