54450afa9ab7149f67b171428dba954a2b00b1fa
Sarpreet Singh <sb223ce@student.lnu.se>
2017-05-18 19:03:26 +0200

    database and admin updated

--- application/admin/src/main/java/com/faceRecognition/admin/service/AdminServiceImpl.java
+++ application/admin/src/main/java/com/faceRecognition/admin/service/AdminServiceImpl.java
@@ -1,27 +1,23 @@
 package com.faceRecognition.admin.service;
 
 import java.util.List;
-
 import javax.naming.directory.InvalidAttributeValueException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.faceRecognition.utils.database.model.UserEntity;
 
 // TODO: add storage/face Service, add pagination support, add database returns userEntity
 public class AdminServiceImpl implements AdminService {
-	
+
 	@Autowired
 	com.faceRecognition.utils.database.service.AdminService databaseService;
 
 	@Override
 	public UserEntity create(MultipartFile file, String personalNumber) throws InvalidAttributeValueException {
-		String link = "linkToFile";		// link = storageService.store(file);
-		String faceId = "1";			// faceId = faceService.save(link)	
-		UserEntity userEntity = null; 	// database should return result
-		databaseService.addUserEntity(faceId, personalNumber);
+		String link = "linkToFile"; // link = storageService.store(file);
+		String faceId = "1"; // faceId = faceService.save(link)
+		UserEntity userEntity = databaseService.addUserEntity(personalNumber, link);
 		return userEntity;
 	}
 
@@ -32,11 +28,10 @@ public class AdminServiceImpl implements AdminService {
 
 	@Override
 	public UserEntity update(MultipartFile file, String id, String personalNumber) throws NotFoundException {
-		String link = "linkToFile";			// link = storageService.store(file);
+		String link = "linkToFile"; // link = storageService.store(file);
 		// faceService.update(id, link);
-		UserEntity userEntity = null;		// database should return result	
-		databaseService.updateUserEntity(id, personalNumber);
-		String faceId = "1";				// faceId = faceService.save(link)
+		UserEntity userEntity = databaseService.updateUserEntity(id, personalNumber, link);
+		String faceId = "1"; // faceId = faceService.save(link)
 		return userEntity;
 	}
 
@@ -48,8 +43,7 @@ public class AdminServiceImpl implements AdminService {
 
 	@Override
 	public List<UserEntity> list(int size, int page) {
-		List<UserEntity> userEntities = databaseService.getUserEntities();	// page and size
+		List<UserEntity> userEntities = databaseService.getUserEntities(); // page and size
 		return userEntities;
 	}
-
 }
--- application/utils/src/main/java/com/faceRecognition/utils/database/service/DatabaseServiceImpl.java
+++ application/utils/src/main/java/com/faceRecognition/utils/database/service/DatabaseServiceImpl.java
@@ -20,50 +20,59 @@ public class DatabaseServiceImpl implements AdminService, UserService, Authentic
 	@Autowired
 	private CredentialsRepository credentialsRepository;
 
+	@Override
 	public String getPersonalNumber(String id) throws NotFoundException {
 		UserEntity entity = this.userEntitiesRepository.findOne(id);
-		if (entity != null)
-			return entity.getPersonalNumber();
-		throw new NotFoundException();
+		if (entity == null)
+			throw new NotFoundException();
+		return entity.getPersonalNumber();
 	}
 
-	// PN Format = YYYYMMDDXXXX
-	public void addUserEntity(String id, String personalNumber) throws InvalidAttributeValueException {
+	@Override // PN Format = YYYYMMDDXXXX
+	public UserEntity addUserEntity(String personalNumber, String photoLink) throws InvalidAttributeValueException {
 		if (personalNumber.length() != 12 || !personalNumber.matches("\\d+"))
 			throw new InvalidAttributeValueException();
-		this.userEntitiesRepository.saveAndFlush(new UserEntity(id, personalNumber));
+		UserEntity entity = new UserEntity(personalNumber, photoLink);
+		this.userEntitiesRepository.saveAndFlush(entity);
+		return entity;
 	}
 
-	public void updateUserEntity(String id, String personalNumber) throws NotFoundException {
+	@Override
+	public UserEntity updateUserEntity(String id, String personalNumber, String photoLink) throws NotFoundException {
 		UserEntity entity = this.userEntitiesRepository.findOne(id);
-		if (entity != null) {
-			entity.setId(id);
-			entity.setPersonalNumber(personalNumber);
-			this.userEntitiesRepository.saveAndFlush(entity);
-		} else
+		if (entity == null)
 			throw new NotFoundException();
+		if (!personalNumber.isEmpty() && personalNumber != null)
+			entity.setPersonalNumber(personalNumber);
+		if (!photoLink.isEmpty() && photoLink != null)
+			entity.setPhotoLink(photoLink);
+		this.userEntitiesRepository.saveAndFlush(entity);
+		return entity;
 	}
 
+	@Override
 	public void deleteUserEntity(String id) throws NotFoundException {
 		UserEntity entity = this.userEntitiesRepository.findOne(id);
-		if (entity != null)
-			this.userEntitiesRepository.delete(entity);
-		else
+		if (entity == null)
 			throw new NotFoundException();
+		this.userEntitiesRepository.delete(entity);
 	}
 
+	@Override
 	public UserEntity getUserEntity(String id) throws NotFoundException {
 		UserEntity entity = this.userEntitiesRepository.findOne(id);
-		if (entity != null)
-			return entity;
-		throw new NotFoundException();
+		if (entity == null)
+			throw new NotFoundException();
+		return entity;
 	}
 
+	@Override
 	public List<UserEntity> getUserEntities() {
 		List<UserEntity> entities = userEntitiesRepository.findAll();
 		return entities == null ? Collections.emptyList() : entities;
 	}
 
+	@Override
 	public boolean isValidCredentials(String username, String password) {
 		Credentials credentials = this.credentialsRepository.findOne(username);
 		return credentials == null ? false : credentials.getPassword().equals(password);
