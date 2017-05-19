f75b4a7c5f122a7ba2b17f3580eb4d2cb7f5337c
kaikun213 <kaktus.w.h@gmx.de>
2017-05-19 22:51:47 +0200

    Admin Dependencies

--- application/admin/src/main/java/com/faceRecognition/admin/Application.java
+++ application/admin/src/main/java/com/faceRecognition/admin/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.faceRecognition.admin.config.JPAConfig;
 import com.faceRecognition.admin.service.AdminServiceImpl;
 import com.faceRecognition.utils.database.repository.UserEntitiesRepository;
 import com.faceRecognition.utils.database.service.DatabaseServiceImpl;
@@ -14,7 +13,7 @@ import com.faceRecognition.utils.storage.service.StorageServiceImpl;
 @SpringBootApplication
 @ComponentScan(basePackageClasses = {Application.class, AdminServiceImpl.class,
 										StorageServiceImpl.class,DatabaseServiceImpl.class, FaceServiceImpl.class,
-										UserEntitiesRepository.class, JPAConfig.class})
+										UserEntitiesRepository.class})
 public class Application {
 	
 	public static void main(String[] args){
