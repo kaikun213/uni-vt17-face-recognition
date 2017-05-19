a543cb6d5cedc0ebfff01bcc293068abe26d01ec
Sarpreet Singh <sb223ce@student.lnu.se>
2017-05-19 13:46:34 +0200

    storage

--- application/admin/src/main/java/com/faceRecognition/admin/service/AdminService.java
+++ application/admin/src/main/java/com/faceRecognition/admin/service/AdminService.java
@@ -1,7 +1,7 @@
 package com.faceRecognition.admin.service;
 
+import java.io.IOException;
 import java.util.List;
-
 import javax.naming.directory.InvalidAttributeValueException;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,11 +9,14 @@ import org.springframework.web.multipart.MultipartFile;
 import com.faceRecognition.utils.database.model.UserEntity;
 
 public interface AdminService {
-	
-	UserEntity create(MultipartFile file, String personalNumber) throws InvalidAttributeValueException;
-	UserEntity retrieve(String id) throws NotFoundException;
-	UserEntity update(MultipartFile file, String id, String personalNumber) throws NotFoundException;
-	void delete(String id) throws NotFoundException;
-	List<UserEntity> list(int size, int page);
 
+	UserEntity create(MultipartFile file, String personalNumber) throws InvalidAttributeValueException, IOException;
+
+	UserEntity retrieve(String id) throws NotFoundException;
+
+	UserEntity update(MultipartFile file, String id, String personalNumber) throws NotFoundException, IOException;
+
+	void delete(String id) throws NotFoundException;
+
+	List<UserEntity> list(int size, int page);
 }
