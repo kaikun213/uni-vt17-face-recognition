4eec5ee28aecfeaa3dc691b1d017014346bbb315
kaikun213 <kaktus.w.h@gmx.de>
2017-05-19 16:58:55 +0200

    application.properties and JPA from utils need to be configured..

--- application/admin/src/main/java/com/faceRecognition/admin/Application.java
+++ application/admin/src/main/java/com/faceRecognition/admin/Application.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.faceRecognition.admin.config.JPAConfig;
 import com.faceRecognition.admin.service.AdminServiceImpl;
+import com.faceRecognition.utils.database.repository.UserEntitiesRepository;
+import com.faceRecognition.utils.database.service.DatabaseServiceImpl;
+import com.faceRecognition.utils.face.service.FaceServiceImpl;
+import com.faceRecognition.utils.storage.service.StorageServiceImpl;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = { Application.class, AdminServiceImpl.class})
+@ComponentScan(basePackageClasses = {Application.class, AdminServiceImpl.class,
+										StorageServiceImpl.class,DatabaseServiceImpl.class, FaceServiceImpl.class,
+										UserEntitiesRepository.class, JPAConfig.class})
 public class Application {
 	
 	public static void main(String[] args){
