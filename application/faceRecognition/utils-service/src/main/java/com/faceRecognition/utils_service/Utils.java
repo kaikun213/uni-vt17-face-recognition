34077e21d3e44a37f9343e1946f015965b7cdf6e
Sarpreet Singh <sb223ce@student.lnu.se>
2017-05-22 00:24:43 +0200

    added face jar and removed uneeded dep

--- application/faceRecognition/utils-service/src/main/java/com/faceRecognition/utils_service/Utils.java
+++ application/faceRecognition/utils-service/src/main/java/com/faceRecognition/utils_service/Utils.java
@@ -1,11 +1,9 @@
 package com.faceRecognition.utils_service;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaAuditing
 public class Utils {
 }
