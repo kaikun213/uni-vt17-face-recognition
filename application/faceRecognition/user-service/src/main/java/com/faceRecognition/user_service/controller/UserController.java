c1b3f747e6d510e3131768286be21535f3ac893e
Sarpreet Singh <sb223ce@student.lnu.se>
2017-05-23 13:58:32 +0200

    minor update user-service

--- application/faceRecognition/user-service/src/main/java/com/faceRecognition/user_service/controller/UserController.java
+++ application/faceRecognition/user-service/src/main/java/com/faceRecognition/user_service/controller/UserController.java
@@ -3,9 +3,9 @@ package com.faceRecognition.user_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.faceRecognition.user_service.service.UserService;
 
@@ -17,7 +17,7 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping
-	public ResponseEntity<String> retrieve(@PathVariable String image) {
+	public ResponseEntity<String> retrieve(@RequestParam(value = "file", required = true) String image) {
 		try {
 			return new ResponseEntity<String>(this.userService.retrieve(image), HttpStatus.OK);
 		} catch (Exception e) {
