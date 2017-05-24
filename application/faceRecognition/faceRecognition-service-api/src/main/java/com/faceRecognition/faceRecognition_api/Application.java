7ba07a456eb497c2132af75b564b455490c9a4d0
kaikun213 <kaktus.w.h@gmx.de>
2017-05-24 20:22:29 +0200

    Enable cross origin

--- application/faceRecognition/faceRecognition-service-api/src/main/java/com/faceRecognition/faceRecognition_api/Application.java
+++ application/faceRecognition/faceRecognition-service-api/src/main/java/com/faceRecognition/faceRecognition_api/Application.java
@@ -3,6 +3,11 @@ package com.faceRecognition.faceRecognition_api;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import com.faceRecognition.admin_service.Admin;
 import com.faceRecognition.user_service.User;
 import com.faceRecognition.utils_service.Utils;
@@ -14,4 +19,15 @@ public class Application {
 		new SpringApplicationBuilder().bannerMode(Banner.Mode.CONSOLE)
 				.sources(Utils.class, Admin.class, User.class, Application.class).run(args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurerAdapter() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/admin").allowedOrigins("*");
+	        }
+	    };
+	}
 }
+
