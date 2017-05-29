027be7ea0af5568cbc7792dbde371e6b31324e3a
Sarpreet Singh <sb223ce@student.lnu.se>
2017-05-29 16:15:59 +0200

    mobile app updated

--- facenummer/src/pages/home/home.ts
+++ facenummer/src/pages/home/home.ts
@@ -20,102 +20,72 @@ export class HomePage {
   constructor(private http: Http, private camera: Camera, private alertCtrl: AlertController, public navCtrl: NavController) { }
 
   logIn(): void {
-    this.isWaitingForResponse = true;
     setTimeout(() => {
-      if (this.username != "user" || this.password != "password") {
-        this.presentAlert('Log in failed!', 'Please try again.');
-        this.isWaitingForResponse = false;
-        return;
-      }
-      this.isLoggedIn = true;
+      if (this.password == "password" && this.username == "user")
+        this.isLoggedIn = true;
+      else this.presentAlert('Log in failed!', 'Please try again.');
       this.isWaitingForResponse = false;
-    }, 500);
+    }, 300);
+    this.isWaitingForResponse = true;
   }
 
   logOut(): void {
-    this.isWaitingForResponse = true;
     setTimeout(() => {
       this.username = "";
       this.password = "";
-      this.isLoggedIn = false;
+      this.isLoggedIn = null;
       this.isWaitingForResponse = false;
-    }, 500);
+    }, 300);
+    this.isWaitingForResponse = true;
   }
 
-  showCheckBox(): void {
+  getPN(): void {
     let alert = this.alertCtrl.create();
     alert.setTitle('How would you like to upload the photo?');
-
-    alert.addInput({
-      type: 'radio',
-      label: 'Use camera',
-      value: 'camera',
-      checked: true
-    });
-
-    alert.addInput({
-      type: 'radio',
-      label: 'Use gallery',
-      value: 'gallery'
-    });
-
+    alert.addInput({ type: 'radio', label: 'Use camera', value: 'camera', checked: true });
+    alert.addInput({ type: 'radio', label: 'Use gallery', value: 'gallery' });
     alert.addButton('Cancel');
     alert.addButton({
       text: 'Okay',
       handler: data => {
-        if (new String(data) == "camera")
-          this.openCamera();
-        else
-          this.openGallery();
+        this.isWaitingForResponse = true;
+        this.camera.getPicture({
+          quality: 100,
+          destinationType: this.camera.DestinationType.DATA_URL,
+          encodingType: this.camera.EncodingType.JPEG,
+          mediaType: this.camera.MediaType.PICTURE,
+          sourceType: data == "camera" ? this.camera.PictureSourceType.CAMERA : this.camera.PictureSourceType.PHOTOLIBRARY
+        }).then(imageData => {
+          this.http.post("https://lnu-face.herokuapp.com/user/", 'file=' + imageData, {
+            headers: this.makeHeaders()
+          }).map((response: Response) => response.json()).subscribe(
+            success => {
+              this.presentAlert('Success', 'PN received!');
+              this.isWaitingForResponse = false;
+              //Todo: display the PN
+            }, err => {
+              this.presentAlert('No result found!', 'Please try again.');
+              this.isWaitingForResponse = false;
+            });
+        }, err => {
+          this.isWaitingForResponse = false;;
+        });
       }
     });
     alert.present();
   }
 
-  postData(image: String): any {
-    let headers: Headers = new Headers();
+  makeHeaders(): Headers {
+    let headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded');
     headers.append('Authorization', 'Basic ' + btoa(this.username + ':' + this.password));
-    return this.http.post("https://lnu-face.herokuapp.com/user/", 'file=' + image, { headers: headers }).map((response: Response) => response.json());
-  }
-
-  openGallery(): void {
-    this.camera.getPicture({
-      destinationType: this.camera.DestinationType.DATA_URL,
-      quality: 100,
-      encodingType: this.camera.EncodingType.JPEG,
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
-    }).then(imageData => setTimeout(this.displayResult(imageData), 5000));
-  }
-
-  openCamera(): void {
-    this.camera.getPicture({
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }).then(imageData => setTimeout(this.displayResult(imageData), 5000));
-  }
-
-  displayResult(imageData) {
-    this.isWaitingForResponse = true;
-    this.postData(imageData).subscribe(
-      result => {
-        this.isWaitingForResponse = false;
-        this.presentAlert('Success', 'PN received!');
-        //Todo: display the PN
-      },
-      err => {
-        this.isWaitingForResponse = false
-        this.presentAlert('No result found!', 'Please contact the admin.');
-      });
+    console.log(btoa(this.username + ':' + this.password));
+    return headers;
   }
 
   presentAlert(title: string, subTitle: string): void {
     let alert = this.alertCtrl.create({
-      title: title,
-      subTitle: subTitle,
-      buttons: ['Ok']
+      title: title, subTitle: subTitle, buttons: ['Ok']
     });
     alert.present();
   }
